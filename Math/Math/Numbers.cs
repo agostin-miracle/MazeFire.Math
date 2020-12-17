6d6e6f8de3e3914ce0202a7e8673f9e8dd5540c4
Agostin Milagres <agostinho.milagres@livre.com.br>
2020-12-17 20:34:58 -0200

    Implementação Inicial da Classe Math

---
+++ Math/Math/Numbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeFire
+{
+    /// <summary>
+    /// Números
+    /// </summary>
+    public static partial class Math
+    {
+        /// <summary>
+        /// Calcula o Fatorial de um número
+        /// </summary>
+        /// <param name="n">Número inteiro</param>
+        /// <returns>int</returns>
+        public static int Fatorial(int n)
+        {
+            int r = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                r *= i;
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// Sequência de Fibonacci
+        /// </summary>
+        /// <param name="n">Iterações</param>
+        /// <returns>o enésimo valor da sequênica de fibonacci</returns>
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
---
+++ Math/Math/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Math")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Math")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("293c651c-390c-4c83-a496-7bf4eecce58b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
